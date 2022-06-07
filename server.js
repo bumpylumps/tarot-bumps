d840a104139dc897064b6db187cd12132c6b908c
bumpyLumps <alexander.fulop.art@gmail.com>
2022-06-07 16:51:09 -0400

    working tarot api

---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require('express')
+const app = express()
+const PORT = 8000
+
+const cards = {
+    'fool': {
+        'name': 'The Fool',
+        'number': 0,
+        'arcana': 'Major'
+    },
+    'magician': {
+        'name': 'The Magician',
+        'number': 1,
+        'arcana': 'Major'
+    }
+}
+
+app.get('/', (request, response)=>{
+    response.sendFile(__dirname + '/index.html')
+})
+
+app.get('/api/:name', (request, response)=>{
+    const cardName = request.params.name.toLowerCase()
+    if(cards[cardName]){
+        response.json(cards[cardName])
+    }
+    
+})
+
+app.listen(PORT, ()=>{
+    console.log(`The server is now running on port ${PORT}, I'm ALIVE!!!`)
+})
